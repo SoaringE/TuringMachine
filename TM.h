d42aece50e42fb814a59966c54c8db494b579133
SoaringE <1305537985@qq.com>
2022-12-03 10:36:01 +0800

    read file

--- TM.h
+++ TM.h
@@ -6,6 +6,7 @@
 #define TURING_TM_H
 
 #include <iostream>
+#include <fstream>
 #include <regex>
 #include <string>
 #include <vector>
--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-#include <fstream>
+#include "TM.h"
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    string syntax;
+    string file_name = "/palindrome_detector_2tapes.tm";
+    ifstream in_file(file_name, ios::in);
+    if (!in_file) {
+        cerr << "Error occurred while opening file " + file_name << endl;
+        exit(-1);
+    }
+    while (!in_file.eof()) {
+        string temp;
+        in_file >> temp;
+        syntax += temp;
+    }
+    in_file.close();
+    cout << syntax << endl;
     return 0;
 }
