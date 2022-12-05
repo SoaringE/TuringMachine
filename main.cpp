3507aa090254fd8f13a6dfb94dd6e77d4475cfa1
SoaringE <1305537985@qq.com>
2022-12-05 11:56:06 +0800

    modify structure

--- main.cpp
+++ main.cpp
@@ -1,13 +1,74 @@
 #include "TM.h"
 
-int main() {
-    string file_name = "../case2.tm";
-    string input = "111111111";
-    bool verbose = true;
+int main(int argc, char* argv[]) {
+    string option;
+    string file_name;
+    string input;
+    bool verbose;
+
+    if (argc == 1) {
+        cerr << "invalid usage" << endl;
+        exit(-1);
+    } else if (argc == 2) {
+        string arg1 = argv[1];
+        if (arg1 == "--help" || arg1 == "-h") {
+            cout << "usage: turing [-v|--verbose] [-h|--help] <tm> <input>" << endl;
+            return 0;
+        } else if (arg1.length() >= 3 && arg1.substr(arg1.length() - 3) == ".tm") {
+            file_name = arg1;
+            verbose = false;
+        }
+        else {
+            cerr << "invalid usage" << endl;
+            exit(-1);
+        }
+    } else if (argc == 3) {
+        string arg1 = argv[1];
+        string arg2 = argv[2];
+        if ((arg1 == "-v" || arg1 == "--verbose") && arg2.length() >= 3 && arg2.substr(arg2.length() - 3) == ".tm") {
+            verbose = true;
+            file_name = arg2;
+        } else if (arg1 == "-h" || arg1 == "--help") {
+            cout << "usage: turing [-v|--verbose] [-h|--help] <tm> <input>" << endl;
+            return 0;
+        } else if (arg1.length() >= 3 && arg1.substr(arg1.length() - 3) == ".tm") {
+            file_name = arg1;
+            input = arg2;
+            verbose = false;
+        } else {
+            cerr << "invalid usage" << endl;
+            exit(-1);
+        }
+    } else if (argc == 4) {
+        string arg1 = argv[1];
+        string arg2 = argv[2];
+        string arg3 = argv[3];
+        if (arg1 == "-h" || arg1 == "--help") {
+            cout << "usage: turing [-v|--verbose] [-h|--help] <tm> <input>" << endl;
+            return 0;
+        } else if ((arg1 == "-v" || arg1 == "--verbose") && arg2.length() >= 3 && arg2.substr(arg2.length() - 3) == ".tm") {
+            file_name = arg2;
+            input = arg3;
+            verbose = true;
+        } else {
+            cerr << "invalid usage" << endl;
+            exit(-1);
+        }
+    } else {
+        string arg1 = argv[1];
+        if (arg1 == "-h" || arg1 == "--help") {
+            cout << "usage: turing [-v|--verbose] [-h|--help] <tm> <input>" << endl;
+            return 0;
+        } else {
+            cerr << "invalid usage" << endl;
+            exit(-1);
+        }
+    }
+
     vector<string> syntax;
     ifstream in_file(file_name, ios::in);
     if (!in_file) {
-        cerr << "Error occurred while opening file " + file_name << endl;
+        cerr << "error occurred while opening file " + file_name << endl;
         exit(-1);
     }
     string line;
